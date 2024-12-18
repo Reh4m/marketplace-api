4be5c8fbc522b8bcdbc5ab4b75d7e2bf50e7d17f
emmarp <ruiizemmanuel@gmail.com>
2024-12-18 16:55:55 -0600

    User, Cart and Address resolvers added to resolvers path

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,17 +1,22 @@
 import { NonEmptyArray } from "type-graphql";
 
+import { AddressResolver } from "./users/addresses.resolver";
+import { UserResolver } from "./users/users.resolver";
+import { CartResolver } from "./users/cart.resolver";
 import { AuthResolver } from "./auth.resolver";
 import { ProductResolver } from "./products.resolver";
-import { UserResolver } from "./users.resolver";
 import { OrderResolver } from "./orders.resolver";
 import { CategoryResolver } from "./categories.resolver";
 import { SaleResolver } from "./sales.resolver";
 import { CouponResolver } from "./coupons.resolver";
 
 export const resolvers: NonEmptyArray<Function> = [
+  // User Resolvers
+  UserResolver,
+  AddressResolver,
+  CartResolver,
   AuthResolver,
   ProductResolver,
-  UserResolver,
   OrderResolver,
   CategoryResolver,
   SaleResolver,
