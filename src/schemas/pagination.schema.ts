eb474c126b255db2d2189e024b1b7ed5c9c836d7
emmarp <ruiizemmanuel@gmail.com>
2024-12-23 23:31:17 -0600

    Feat: Implement pagination, sorting, and filtering validation schema

---
+++ src/schemas/pagination.schema.ts
@@ -0,0 +1,30 @@
+import { ArgsType, Field, Int } from "type-graphql";
+import { IsNumber, Max, Min, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+
+import { SortProductsInput } from "./sort.schema";
+import { FilterProductsInput } from "./filter.schema";
+
+@ArgsType()
+export class GetProductsArgs {
+  @Field((_type) => Int)
+  @IsNumber()
+  @Min(0)
+  skip: number;
+
+  @Field((_type) => Int)
+  @IsNumber()
+  @Min(1)
+  @Max(50)
+  take: number;
+
+  @Field((_type) => SortProductsInput)
+  @ValidateNested()
+  @Type(() => SortProductsInput)
+  sort: SortProductsInput;
+
+  @Field((_type) => FilterProductsInput, { nullable: true })
+  @ValidateNested()
+  @Type(() => FilterProductsInput)
+  filter?: FilterProductsInput;
+}
